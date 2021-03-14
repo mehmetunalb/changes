dcdc972db168641afc2dcd8a608d4af93486773e
techtorial <50930535+techtorial@users.noreply.github.com>
2021-03-14 12:50:01 -0500

    Added new name
    new name

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Techtorial");
         System.out.println("Ahmed");
+        System.out.println("Muammer");
     }
 }
